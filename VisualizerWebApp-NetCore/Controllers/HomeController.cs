1e059ec4c1f7544c244c0daf4612b9e407a13514
Donna Malayeri <donnam-msft@outlook.com>
2017-06-14 13:39:00 -0700

    Add error handling and dot file download

--- VisualizerWebApp-NetCore/Controllers/HomeController.cs
+++ VisualizerWebApp-NetCore/Controllers/HomeController.cs
@@ -27,21 +27,34 @@ namespace VisualizerWebApp.Controllers
         }
 
         [HttpPost("UploadFiles")]
-        public async Task<IActionResult> UploadFile(List<IFormFile> files)
+        public async Task<IActionResult> UploadFile(IFormFile formFile)
         {
             var filePath = Path.GetTempFileName();
 
-            foreach (var formFile in files) {
+            if (Path.GetExtension(formFile.FileName) != "zip") {
+                return await ShowError("A .zip file is required");
+            }
+
+            try {
                 if (formFile.Length > 0) {
                     using (var stream = new FileStream(filePath, FileMode.Create)) {
                         await formFile.CopyToAsync(stream);
                     }
                 }
+
+                string fileContent = ZipDirToRenderedFile(filePath);
+            }
+            catch (Exception e) {
+                return await ShowError($"Error processing zipfile: {e.Message}");
             }
 
-            string fileContent = ZipDirToRenderedFile(filePath);
-
             return View("ViewImage");
+        }
+
+        private async Task<IActionResult> ShowError(string message)
+        {
+            await Response.WriteAsync($"<script language=\"JavaScript\">alert('{message}')</script>");
+            return new EmptyResult();
         }
 
         private string ZipDirToRenderedFile(string filePath)
@@ -63,26 +76,37 @@ namespace VisualizerWebApp.Controllers
         [HttpPost("GitHubContent")]
         public async Task<IActionResult> DownloadGitHubRepo(string repoUrl)
         {
-            string archiveUrl = $"{repoUrl}/archive/master.zip";
-
-            var client = new HttpClient();
-            var content = await client.GetAsync(archiveUrl);
-            var zipfile = GetTempFilePath();
-
-            using (var fileStream = new FileStream(zipfile, FileMode.Create, FileAccess.Write, FileShare.None)) {
-                await content.Content.CopyToAsync(fileStream);
+            try {
+                var uri = new Uri(repoUrl);
+            }
+            catch (UriFormatException) {
+                return await ShowError("Invalid URL");
             }
 
-            string fileContent = ZipDirToRenderedFile(zipfile);
+            try {
+                string archiveUrl = $"{repoUrl}/archive/master.zip";
+
+                var client = new HttpClient();
+                var content = await client.GetAsync(archiveUrl);
+                var zipfile = GetTempFilePath();
+
+                using (var fileStream = new FileStream(zipfile, FileMode.Create, FileAccess.Write, FileShare.None)) {
+                    await content.Content.CopyToAsync(fileStream);
+                }
+
+                string fileContent = ZipDirToRenderedFile(zipfile);
+            }
+            catch (Exception e) {
+                return await ShowError($"Error processing repo: {e.Message}");
+            }
 
             return View("ViewImage");
         }
 
-        public IActionResult DownloadDotFile()
+        public IActionResult DownloadDotFile(string file)
         {
-            var dotFile = new FileInfo(ViewData["DotFile"].ToString());
-
-            return File(dotFile.FullName, "application/octet-stream");
+            var bytes = System.IO.File.ReadAllBytes(file);
+            return File(bytes, "application/octet-stream", "output.dot");
         }
 
         private string CreateOutputFile(string filename, string format)
@@ -96,7 +120,7 @@ namespace VisualizerWebApp.Controllers
         private string CreateDotFile(string directory)
         {
             var dotFile = GetTempFilePath();
-            Visualizer.DotFileFromFunctionDirectory(directory, dotFile);        
+            Visualizer.DotFileFromFunctionDirectory(directory, dotFile);
             return dotFile;
         }
 
