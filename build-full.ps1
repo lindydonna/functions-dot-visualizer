59a5cdc4d8cc362a064b21cc00f2a11a8b2cdbd0
Donna Malayeri <lindydonna@gmail.com>
2017-06-16 12:47:32 -0700

    Update dockerfile

--- build-full.ps1
+++ build-full.ps1
@@ -1,6 +1,6 @@
 param($dir, $dotFile)
 
-. "D:\git-repos\functions-dot-visualizer\functions-dot-visualizer\bin\Debug\FunctionsDotVisualizer.exe" $dir $dotFile
+. "D:\git-repos\functions-dot-visualizer\VisualizerConsoleApp\bin\Debug\FunctionsDotVisualizer.exe" $dir $dotFile
 
 $outputFilename = $dotFile.substring(0, $dotFile.indexOf(".")) + ".svg"
 
