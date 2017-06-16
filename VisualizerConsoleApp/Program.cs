bbbea14a62f401beab7ddc99e92512818552cc28
Donna Malayeri <lindydonna@gmail.com>
2017-06-15 17:47:44 -0700

    Add console app

---
+++ VisualizerConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using DotVisualizerLib;
+
+namespace FunctionsDotVisualizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Visualizer.DotFileFromFunctionDirectory(args[0], args[1]);
+        }
+    }
+}
